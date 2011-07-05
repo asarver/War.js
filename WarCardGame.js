901e08e90c9d219fbd301fe4ef41c7bb2baf0dbc
Ashley Sarver <asarver1@gmail.com>
2011-07-05 05:44:31 -0400

    Creates a deck and logs it to the console

--- WarCardGame.js
+++ WarCardGame.js
@@ -1,79 +1,87 @@
+var myDeck = new Array();
+
 function War() {
-  
   // vars used for game
-  this.deck = new Array(52);
-  this.compDeck = 0;
-  this.myDeck = 0;
+  //myDeck = new Array(52);
+  //this.compDeck = 0;
+  //this.myDeck = 0;
+}
+
+War.prototype = new War();
   
-  // functions used to create deck and play game
-  this.prototype.createDeck = function() {
-    // TODO: create a Deck of 52 cards
-    var aDeck = new Array(52);
-    var index = 0;
-    for(var x = 1; x <= 13; ++x) {
-      if (x == 1) {
-        for (var y = 0; y < 4; ++y) {
-          aDeck[index] = 'Ace of ' + char(y);
-          ++index;
-        }
-      } else if (x == 11) {
-        for (var y = 0; y < 4; ++y) {
-          aDeck[index] = 'Jack of ' + char(y);
-          ++index;
-        }
-      } else if (x == 12) {
-        for (var y = 0; y < 4; ++y) {
-          aDeck[index] = 'Queen of ' + char(y);
-          ++index;
-        }
-      } else if (x == 13) {
-        for (var y = 0; y < 4; ++y) {
-          aDeck[index] = 'King of ' + char(y);
-          ++index;
-        }
-      } else {
-        for (var y = 0; y < 4; ++y) {
-          aDeck[index] = x + '\'s of ' + char(y);
-          ++index;
-        }
+// functions used to create deck and play game
+War.prototype.createDeck = function() {
+  var index = 0;
+  for(var x = 1; x <= 13; ++x) {
+    if (x == 1) {
+      for (var y = 0; y < 4; ++y) {
+        myDeck[index] = 'Ace of ' + y;
+        ++index;
+        //console.log('In the Aces');
+      }
+    } else if (x == 11) {
+      for (var y = 0; y < 4; ++y) {
+        myDeck[index] = 'Jack of ' + y;
+        ++index;
+        //console.log('In the Jacks');
+      }
+    } else if (x == 12) {
+      for (var y = 0; y < 4; ++y) {
+        myDeck[index] = 'Queen of ' + y;
+        ++index;
+        //console.log('In the Queens');
+      }
+    } else if (x == 13) {
+      for (var y = 0; y < 4; ++y) {
+        myDeck[index] = 'King of ' + y;
+        ++index;
+        //console.log('In the Kings');
+      }
+    } else {
+      for (var y = 0; y < 4; ++y) {
+        myDeck[index] = x + '\'s of ' + y;
+        ++index;
+        //console.log('In the regular numbers');
       }
     }
-    return aDeck;
   }
+  console.log('Done.');
+}
   
-  this.prototype.shuffleDeck = function() {
-    // TODO: shuffles deck
-  }
+War.prototype.shuffleDeck = function() {
+  // TODO: shuffles deck
+}
   
-  this.prototype.splitDeck = function() {
-    // TODO: splits deck between compDeck and myDeck
-  }
+War.prototype.splitDeck = function() {
+  // TODO: splits deck between compDeck and myDeck
+}
   
-  this.prototype.drawCards = function() {
-    // TODO: draws a card from compDeck and myDeck
-  }
+War.prototype.drawCards = function() {
+  // TODO: draws a card from compDeck and myDeck
+}
   
-  this.prototype.compareCards = function() {
-    // TODO: compares the cards drawn
-  }
+War.prototype.compareCards = function() {
+  // TODO: compares the cards drawn
+}
   
-  this.prototype.war = function() {
-    // TODO: if cards are equal, another two cards are drawn
-  }
+War.prototype.war = function() {
+  // TODO: if cards are equal, another two cards are drawn
+}
   
-  this.prototype.addCardsToDeck = function() {
-    // TODO: adds cards to winner's deck
-  }
+War.prototype.addCardsToDeck = function() {
+  // TODO: adds cards to winner's deck
+}
   
-  this.prototype.display = function() {
-    // TODO: displays the cards on the table
-    // idk if I need this yet
-    for (var i = 0; i < this.deck; ++i) {
-      console.log(this.deck[i]);
-    }
+War.prototype.display = function() {
+  // TODO: displays the cards on the table
+  // idk if I need this yet
+  console.log('I am in display function');
+  for (var i = 0; i < myDeck.length; ++i) {
+    console.log(myDeck[i]);
   }
 }
 
-var myObject = War();
-myObject.deck = createDeck();
+
+var myObject = new War();
+myObject.createDeck();
 myObject.display();
